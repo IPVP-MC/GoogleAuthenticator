11ab18703390db7012c3ad145921640808fe28e1
Matthew <matthew.steglinski@utoronto.ca>
2016-01-21 19:13:22 -0500

    Implement /auth off, ipenable, and ipdisable subcommands

--- src/main/java/org/originmc/googleauthenticator/commands/AuthCommand.java
+++ src/main/java/org/originmc/googleauthenticator/commands/AuthCommand.java
@@ -27,30 +27,55 @@ public class AuthCommand extends Command {
     public void execute(CommandSender sender, String[] args) {
         if (!(sender instanceof ProxiedPlayer)) {
             sender.sendMessage(new TextComponent("Only players can use this command"));
-        } else if (args.length == 0) {
+        } else {
             ProxiedPlayer player = (ProxiedPlayer) sender;
-
-            // Create new authentication conversation
-            Conversation conversation = new Conversation(plugin, (ProxiedPlayer) sender, new AuthenticationBeginPrompt(plugin));
-            conversation.getContext().setSessionData("authdata",
-                    new AuthenticationData(AuthenticatorCodeUtils.generateNewSecret(),
-                    player.getAddress().getAddress().getHostName()));
-            conversation.addConversationCanceller((context, input) -> {
-                if (input.equalsIgnoreCase("exit")) {
-                    TextComponent cancelMessage = new TextComponent("Cancelled two-factor setup process! Try again soon.");
-                    cancelMessage.setColor(ChatColor.RED);
-                    context.getForWhom().sendMessage(cancelMessage);
-                    return true;
+            if (args.length == 0) {
+                // Create new authentication conversation
+                Conversation conversation = new Conversation(plugin, (ProxiedPlayer) sender, new AuthenticationBeginPrompt(plugin));
+                conversation.getContext().setSessionData("authdata",
+                        new AuthenticationData(AuthenticatorCodeUtils.generateNewSecret(),
+                                player.getAddress().getAddress().getHostName()));
+                conversation.addConversationCanceller((context, input) -> {
+                    if (input.equalsIgnoreCase("exit")) {
+                        TextComponent cancelMessage = new TextComponent("Cancelled two-factor setup process! Try again soon.");
+                        cancelMessage.setColor(ChatColor.RED);
+                        context.getForWhom().sendMessage(cancelMessage);
+                        return true;
+                    }
+                    return false;
+                });
+                conversation.begin();
+            } else if (args[0].equalsIgnoreCase("off")) {
+                if (!plugin.hasAuthenticationData(player.getUniqueId())) {
+                    player.sendMessage(TextComponent.fromLegacyText(ChatColor.RED + "You need to authenticate yourself first!"));
+                } else {
+                    plugin.getProxy().getScheduler().runAsync(plugin, () -> {
+                        plugin.removeAuthenticationData(player.getUniqueId());
+                        player.sendMessage(TextComponent.fromLegacyText(ChatColor.GREEN + "Disabled two-factor " +
+                                "authentication! You can still enable it again by running \"/auth\""));
+                    });
                 }
-                return false;
-            });
-            conversation.begin();
-        } else if (args[0].equalsIgnoreCase("off")) {
-
-        } else if (args[0].equalsIgnoreCase("ipenable")) {
-
-        } else if (args[0].equalsIgnoreCase("ipdisable")) {
-
+            } else if (args[0].equalsIgnoreCase("ipenable")) {
+                if (!plugin.hasAuthenticationData(player.getUniqueId())) {
+                    player.sendMessage(TextComponent.fromLegacyText(ChatColor.RED + "You need to authenticate yourself first!"));
+                } else {
+                    AuthenticationData data = plugin.getAuthenticationData(player.getUniqueId());
+                    data.setIpTrusted(true);
+                    player.sendMessage(TextComponent.fromLegacyText(ChatColor.GREEN + "We will now remember you by your " +
+                            "IP and will no longer ask for a code every time you log in!"));
+                }
+            } else if (args[0].equalsIgnoreCase("ipdisable")) {
+                if (!plugin.hasAuthenticationData(player.getUniqueId())) {
+                    player.sendMessage(TextComponent.fromLegacyText(ChatColor.RED + "You need to authenticate yourself first!"));
+                } else {
+                    AuthenticationData data = plugin.getAuthenticationData(player.getUniqueId());
+                    data.setIpTrusted(false);
+                    player.sendMessage(TextComponent.fromLegacyText(ChatColor.GREEN + "We will no longer remember you by your " +
+                            "IP and will now require a code every time you log in!"));
+                }
+            } else {
+                player.sendMessage(TextComponent.fromLegacyText(ChatColor.RED + "Unknown sub command."));
+            }
         }
     }
 }
